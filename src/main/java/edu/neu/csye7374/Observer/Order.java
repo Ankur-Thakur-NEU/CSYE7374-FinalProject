fdaa06b434707bfdc9f871424a23308f816827aa
Saitarun movva <movva.sa@northeastern.edu>
2023-04-24 10:48:08 -0400

    adding the observer package and classes

--- src/main/java/edu/neu/csye7374/Observer/Order.java
+++ src/main/java/edu/neu/csye7374/Observer/Order.java
@@ -1,8 +1,35 @@
 package edu.neu.csye7374.Observer;
 
+import edu.neu.csye7374.Facade.DeliveryType;
+import edu.neu.csye7374.MedicineAPI;
 import edu.neu.csye7374.State.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order implements OrderStateAPI {
+    private int id;
+
+    private double orderCost=0;
+
+    private int count=0;
+
+    private double discount=0;
+
+    private double shippingCost;
+
+
+
+
+
+    private List<MedicineAPI> medicineList = new ArrayList<>();
+    private DeliveryType deliveryType;
+    private static int counter=0;
+    private List<OrderObserver> observers = new ArrayList<>();
+    {
+        observers.add(new PriceObserver());
+        observers.add(new QuantityObserver());
+    }
     private OrderStateAPI state;
     private OrderStateAPI startOrder= new StartOrder(this);
     private OrderStateAPI orderConfirmed = new OrderConfirmed(this);
@@ -12,7 +39,36 @@ public class Order implements OrderStateAPI {
 
     public Order() {
         super();
+        this.id = ++counter;
+        this.deliveryType = DeliveryType.Pickup;
         this.state = getStartOrder();
+    }
+    public Order(double orderCost, int count, double shippingCost, DeliveryType deliveryType) {
+        super();
+        this.id = ++counter;
+        this.orderCost = orderCost;
+        this.count = count;
+        this.deliveryType = deliveryType;
+
+        if(deliveryType==DeliveryType.Pickup) {
+            this.shippingCost = 0;
+        }else {
+            this.shippingCost = shippingCost;
+        }
+        this.state = getStartOrder();
+    }
+    public void addMedicine(MedicineAPI medicine) {
+        medicineList.add(medicine);
+        orderCost+=medicine.medicinePrice();
+        count++;
+        observers.forEach(o->o.updated(this));
+    }
+
+    public boolean removeMedicine(MedicineAPI book) {
+        if(medicineList.remove(book)) {
+            return true;
+        }
+        return false;
     }
 
 
@@ -83,4 +139,113 @@ public class Order implements OrderStateAPI {
     public void setCloseOrder(OrderStateAPI closeOrder) {
         this.closeOrder = closeOrder;
     }
+    public List<MedicineAPI> getMedicineList() {
+        return medicineList;
+    }
+
+
+    public void setMedicineList(List<MedicineAPI> bookList) {
+        this.medicineList = bookList;
+    }
+
+    public String getMedicineListString() {
+        StringBuilder sb = new StringBuilder();
+        for(MedicineAPI b : medicineList) {
+            sb.append(b);
+            sb.append("; ");
+        }
+        return sb.toString();
+    }
+    public DeliveryType getDeliveryType() {
+        return deliveryType;
+    }
+
+
+    public void setDeliveryType(DeliveryType deliveryType) {
+        this.deliveryType = deliveryType;
+    }
+    public static int getCounter() {
+        return counter;
+    }
+
+
+    public static void setCounter(int counter) {
+        Order.counter = counter;
+    }
+
+
+    public List<OrderObserver> getObservers() {
+        return observers;
+    }
+
+
+    public void setObservers(List<OrderObserver> observers) {
+        this.observers = observers;
+    }
+
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+
+    public double getOrderCost() {
+        return orderCost;
+    }
+
+    public void attach(OrderObserver observer) {
+        observers.add(observer);
+    }
+
+    public void detach(OrderObserver observer) {
+        observers.remove(observer);
+    }
+
+    public Order(int id) {
+        this.id=id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getTotal() {
+        return orderCost - discount + shippingCost;
+    }
+
+    public void setOrderCost(double orderCost) {
+        this.orderCost = orderCost;
+    }
+
+    public int getCount() {
+        return medicineList.size();
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public double getShippingCost() {
+        return shippingCost;
+    }
+
+    public void setShippingCost(double shippingCost) {
+        this.shippingCost = shippingCost;
+    }
+
+    @Override
+    public String toString() {
+        return "Order [id=" + id + ", itemCost=" + orderCost + ", count=" + count + ", discount=" + discount
+                + ", shippingCost=" + shippingCost+ "]";
+    }
+
+
 }
--- src/main/java/edu/neu/csye7374/Observer/QuantityObserver.java
+++ src/main/java/edu/neu/csye7374/Observer/QuantityObserver.java
@@ -1,8 +1,10 @@
 package edu.neu.csye7374.Observer;
 
-public class QuantityObserver {
+import edu.neu.csye7374.Facade.DeliveryType;
+
+public class QuantityObserver implements OrderObserver {
     public void updated(Order order) {
-        if(order.getDeliveryType()==DeliveryType.Delivery) {
+        if(order.getDeliveryType()== DeliveryType.Delivery) {
             int count=order.getCount();
             double total = order.getCount();
             if(count<=5) {
